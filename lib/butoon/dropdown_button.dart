143cb1c1a274b7fdad4c1cb013e9601396d71972
rizki <rizkiadnanfutuh@gmail.com>
2024-09-18 04:35:15 +0700

    up

---
+++ lib/butoon/dropdown_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExample();
+}
+
+class _DropdownButtonExample extends State<DropdownButtonExample> {
+  String? language;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: DropdownButton<String>(
+        items: const <DropdownMenuItem<String>>[
+          DropdownMenuItem<String>(
+            value: 'Dart',
+            child: Text('Dart'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Kotlin',
+            child: Text('Kotlin'),
+          ),
+          DropdownMenuItem<String>(
+            value: 'Swift',
+            child: Text('Swift'),
+          ),
+        ],
+        value: language,
+        hint: const Text('Select Language'),
+        onChanged: (String? value) {
+          setState(() {
+            language = value;
+          });
+        },
+      ),
+    );
+  }
+}
