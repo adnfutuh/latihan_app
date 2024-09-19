e310a4c0fdd6df66818042cf74b00d71eaa00bba
rizki <rizkiadnanfutuh@gmail.com>
2024-09-20 03:55:07 +0700

    up

---
+++ lib/listview/navigation/navigation_pop.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  final String message;
+  const SecondScreen(this.message, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(message),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Kembali'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
