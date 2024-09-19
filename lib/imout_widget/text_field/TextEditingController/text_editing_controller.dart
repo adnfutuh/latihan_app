2c193c877625e914d08b35b6d7785406190e49ce
rizki <rizkiadnanfutuh@gmail.com>
2024-09-19 19:51:09 +0700

    up

---
+++ lib/imout_widget/text_field/TextEditingController/text_editing_controller.dart
@@ -0,0 +1,11 @@
+// TextEditingController _controller = TextEditingController();
+ 
+// TextField(
+//   controller: _controller,
+// ),
+
+// @override
+// void dispose() {
+//   _controller.dispose();
+//   super.dispose();
+// }
