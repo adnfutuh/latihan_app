761f94995a993adde77f284421b764b7edaaad73
rizki <rizkiadnanfutuh@gmail.com>
2024-09-19 19:50:59 +0700

    up

---
+++ lib/imout_widget/text_field/text_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TextFieldExample extends StatefulWidget {
+  const TextFieldExample({super.key});
+
+  @override
+  State<TextFieldExample> createState() => _TextFieldExampleState();
+}
+
+class _TextFieldExampleState extends State<TextFieldExample> {
+  String _name = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _name = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text('Hello, $_name'),
+                    );
+                  },
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
