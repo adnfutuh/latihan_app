b583003d89b4047543bc64480a69cfa03831f190
rizki <rizkiadnanfutuh@gmail.com>
2024-09-20 00:06:21 +0700

    up

--- lib/listview/dinamis/list.dart
+++ lib/listview/dinamis/list.dart
@@ -7,23 +7,23 @@ class ScrollingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return Scaffold(
-      body: ListView(
-        children: numberList.map((number) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
+        body: ListView(
+            children: numberList.map(
+      (number) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            border: Border.all(color: Colors.black),
+          ),
+          child: Center(
+            child: Text(
+              '$number',
+              style: const TextStyle(fontSize: 50),
             ),
-            child: Center(
-              child: Text(
-                '$number', // Ditampilkan sesuai item
-                style: const TextStyle(fontSize: 50),
-              ),
-            ),
-          );
-        }).toList(),
-      ),
-    );
+          ),
+        );
+      },
+    ).toList()));
   }
 }
