8d6ba0479cdfa8c49d27d67311a0cf636a6e36ff
rizki <rizkiadnanfutuh@gmail.com>
2024-09-20 00:06:31 +0700

    up

---
+++ lib/listview/listview_separated/listview_separate.dart
@@ -0,0 +1,21 @@
+// ListView.separated(
+//   itemCount: numberList.length,
+//   itemBuilder: (BuildContext context, int index) {
+//     return Container(
+//       height: 250,
+//       decoration: BoxDecoration(
+//         color: Colors.grey,
+//         border: Border.all(color: Colors.black),
+//       ),
+//       child: Center(
+//         child: Text(
+//           '${numberList[index]}',
+//           style: const TextStyle(fontSize: 50),
+//         ),
+//       ),
+//     );
+//   },
+//   separatorBuilder: (BuildContext context, int index) {
+//     return const Divider();
+//   },
+// ),
