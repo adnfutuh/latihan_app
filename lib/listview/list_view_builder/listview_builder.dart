679516d6b3623831a95019197eace15bd9e1edcf
rizki <rizkiadnanfutuh@gmail.com>
2024-09-20 00:06:26 +0700

    up

---
+++ lib/listview/list_view_builder/listview_builder.dart
@@ -0,0 +1,18 @@
+// ListView.builder(
+//   itemCount: numberList.length,
+//   itemBuilder: (BuildContext context, int index) {
+//     return Container(
+      // height: 250,
+      // decoration: BoxDecoration(
+      //   color: Colors.grey,
+      //   border: Border.all(color: Colors.black),
+      // ),
+      // child: Center(
+      //   child: Text(
+      //     '${numberList[index]}',
+      //     style: const TextStyle(fontSize: 50),
+      //   ),
+      // ),
+//     );
+//   },
+// ),
